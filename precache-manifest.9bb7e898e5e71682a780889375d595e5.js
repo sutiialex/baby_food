3f80219ace3c8ea2e27e3036f8a6395c4cdeb366
Alexandru Sutii <alex@othera.io>
2020-04-10 14:43:56 +0200

    Updates

---
+++ precache-manifest.9bb7e898e5e71682a780889375d595e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4412e2be923fe790081f752e7ea81750",
+    "url": "/baby_food/index.html"
+  },
+  {
+    "revision": "d2cb1ace4f39cd99c029",
+    "url": "/baby_food/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9a57bc3266571ac4b2d4",
+    "url": "/baby_food/static/js/2.290da9f1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/baby_food/static/js/2.290da9f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2cb1ace4f39cd99c029",
+    "url": "/baby_food/static/js/main.89ac146b.chunk.js"
+  },
+  {
+    "revision": "e1951b803b998a04b4d4",
+    "url": "/baby_food/static/js/runtime-main.1692884c.js"
+  }
+]);
